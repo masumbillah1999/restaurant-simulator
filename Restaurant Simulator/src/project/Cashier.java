2888ededd3bdb88ee2fc12bbaccaa38c337f83a0
Masum Billah <Masum Billah@DESKTOP-AEI11M4>
2016-05-24 22:58:29 -0400

    hi

--- Restaurant Simulator/src/project/Cashier.java
+++ Restaurant Simulator/src/project/Cashier.java
@@ -3,11 +3,14 @@ package project;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Toolkit;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
 
 public class Cashier extends JDialog {
 
@@ -33,6 +36,15 @@ public class Cashier extends JDialog {
 		setResizable(false);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setIcon(new ImageIcon("/project/20160205_224822.jpg"));
+		
+		lblNewLabel.setBounds(39, 27, 374, 202);
+		getContentPane().add(lblNewLabel);
+		
+		add(getContentPane());
+		validate();
+		
 	}
-
 }
