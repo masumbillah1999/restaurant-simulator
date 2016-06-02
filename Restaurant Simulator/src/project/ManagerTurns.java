3cf10c454f6ecb318c1974ccd52fc1ddd7414a97
322936881 <322936881@C-BP2FC4J.tdsb.on.ca>
2016-06-02 15:36:52 -0400

    Comments finished on managerTurns, still needs tweaking/balancing/window hiding

--- Restaurant Simulator/src/project/ManagerTurns.java
+++ Restaurant Simulator/src/project/ManagerTurns.java
@@ -3,7 +3,7 @@
  * 342149929 322936881
  * 02/06/16
  * Restaurant Simulator ManagerTurns program
- * This method runs the managers turns, including all calculations and openning/closing windows
+ * This method runs the managers turns, including all calculations and opening/closing windows
  */
 
 package project;
@@ -32,14 +32,11 @@ import java.util.Random;
 
 public class ManagerTurns extends JDialog {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private static ButtonGroup managerGroup = new ButtonGroup(); // button group
+	private static ButtonGroup managerGroup = new ButtonGroup(); // button group is declared
 
 	/**
-	 * Launch the application.
+	 * Launches the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -56,172 +53,182 @@ public class ManagerTurns extends JDialog {
 	}
 
 	/**
-	 * Create the dialog.
+	 * Creates the dialog.
 	 */
 	
-	public static int budget = 10000, satisfaction = 50, fame = 50;
-	public static String event;
+	public static int budget = 10000, satisfaction = 50, fame = 50;//budget, satisfaction and fame are declared as global variables for use throughout the code
+	public static String event;//event variable is declared
 	
+	/**
+	 * The constructor method for the class
+	 */
 	public ManagerTurns() {
+		setResizable(false);
 
 		
 		setIconImage(
-				Toolkit.getDefaultToolkit().getImage(ManagerTurns.class.getResource("/project/restaurant logo.png")));
-		setTitle("Manager Game");
-		setBounds(100, 100, 449, 413);
+				Toolkit.getDefaultToolkit().getImage(ManagerTurns.class.getResource("/project/restaurant logo.png")));//sets icon for the window
+		setTitle("Manager Game");//sets the title for the window
+		setBounds(100, 100, 503, 413);//The size of the game window
 		getContentPane().setLayout(null);
 
-		String[] storeItems = new String[] { "Potted Plants", "Higher quality food", "Provide free meals to kids", "Hold a charity fundraiser", "Sponser local sports team", "Employee in a mascot costume", "Newspaper article", "Fancy table cloths", "Create a website", "Forge Yelp reviews" };
+		String[] storeItems = new String[] { "Potted Plants", "Higher quality food", "Provide free meals to kids", "Hold a charity fundraiser", "Sponser local sports team", "Employee in a mascot costume", "Newspaper article", "Fancy table cloths", "Create a website", "Forge Yelp reviews" };//the names for all of the items that can be bought at the store
 		
 		
 		JTextPane txtpnYourCurrentBudget = new JTextPane();
-		txtpnYourCurrentBudget.setEditable(false);
-		txtpnYourCurrentBudget.setText("Your current budget: " + budget + "$");
-		txtpnYourCurrentBudget.setBounds(10, 11, 175, 20);
-		getContentPane().add(txtpnYourCurrentBudget);
+		txtpnYourCurrentBudget.setEditable(false);//makes the text box un-editable
+		txtpnYourCurrentBudget.setText("Your current budget: " + budget + "$");//displays budget
+		txtpnYourCurrentBudget.setBounds(10, 11, 175, 20);//size of the text box
+		getContentPane().add(txtpnYourCurrentBudget);//adds text to the text box
 
 		JTextPane txtpnSatisfaction = new JTextPane();
-		txtpnSatisfaction.setEditable(false);
-		txtpnSatisfaction.setText("Satisfaction: " + satisfaction);
-		txtpnSatisfaction.setBounds(195, 11, 93, 20);
-		getContentPane().add(txtpnSatisfaction);
+		txtpnSatisfaction.setEditable(false);//makes the text box un-editable
+		txtpnSatisfaction.setText("Satisfaction: " + satisfaction);//displays satisfaction
+		txtpnSatisfaction.setBounds(195, 11, 93, 20);//size of the text box
+		getContentPane().add(txtpnSatisfaction);//adds text to the text box
 
 		JTextPane txtpnFame = new JTextPane();
-		txtpnFame.setEditable(false);
-		txtpnFame.setText("Fame:" + fame);
-		txtpnFame.setBounds(315, 11, 93, 20);
-		getContentPane().add(txtpnFame);
+		txtpnFame.setEditable(false);//makes the text box un-editable
+		txtpnFame.setText("Fame:" + fame);//displays fame
+		txtpnFame.setBounds(315, 11, 93, 20);//size of the text box
+		getContentPane().add(txtpnFame);//adds text to the text box
 
 		JTextPane txtpnWelcomeToThe = new JTextPane();
-		txtpnWelcomeToThe.setEditable(false);
-		txtpnWelcomeToThe.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtpnWelcomeToThe.setText("Welcome to the manager store, check all the things you wish to purchase, then press next.");
-		txtpnWelcomeToThe.setBounds(10, 42, 414, 38);
-		getContentPane().add(txtpnWelcomeToThe);
+		txtpnWelcomeToThe.setEditable(false);//makes the text box un-editable
+		txtpnWelcomeToThe.setFont(new Font("Tahoma", Font.PLAIN, 13));//stes font for the text box
+		txtpnWelcomeToThe.setText("Welcome to the manager store, check all the things you wish to purchase, then press next.");//displays intro/instructions
+		txtpnWelcomeToThe.setBounds(10, 42, 414, 38);//size of the text box
+		getContentPane().add(txtpnWelcomeToThe);//adds text to the text box
 
 		
 	
+		//radio buttons are declared and sized
+		JRadioButton rdbtnPottedPlants = new JRadioButton("Potted Plants (500$)");
+		rdbtnPottedPlants.setBounds(6, 87, 231, 23);
+		getContentPane().add(rdbtnPottedPlants);
 
-		JRadioButton radioButton = new JRadioButton(storeItems[0]);
-		radioButton.setBounds(20, 87, 186, 23);
-		getContentPane().add(radioButton);
+		JRadioButton rdbtnHigherQualityFood = new JRadioButton("Higher quality food (600$)");
+		rdbtnHigherQualityFood.setBounds(6, 116, 231, 23);
+		getContentPane().add(rdbtnHigherQualityFood);
 
-		JRadioButton radioButton_1 = new JRadioButton(storeItems[1]);
-		radioButton_1.setBounds(20, 116, 186, 23);
-		getContentPane().add(radioButton_1);
+		JRadioButton rdbtnProvideFreeMeals = new JRadioButton("Provide free meals to kids (1000$)");
+		rdbtnProvideFreeMeals.setBounds(6, 144, 231, 23);
+		getContentPane().add(rdbtnProvideFreeMeals);
 
-		JRadioButton radioButton_2 = new JRadioButton(storeItems[2]);
-		radioButton_2.setBounds(20, 144, 186, 23);
-		getContentPane().add(radioButton_2);
+		JRadioButton rdbtnHoldACharity = new JRadioButton("Hold a charity fundraiser (1500$)");
+		rdbtnHoldACharity.setBounds(6, 170, 231, 23);
+		getContentPane().add(rdbtnHoldACharity);
 
-		JRadioButton radioButton_3 = new JRadioButton(storeItems[3]);
-		radioButton_3.setBounds(20, 170, 186, 23);
-		getContentPane().add(radioButton_3);
+		JRadioButton rdbtnSponserLocalSports = new JRadioButton("Sponser local sports team (2000$)");
+		rdbtnSponserLocalSports.setBounds(6, 196, 231, 23);
+		getContentPane().add(rdbtnSponserLocalSports);
 
-		JRadioButton radioButton_4 = new JRadioButton(storeItems[4]);
-		radioButton_4.setBounds(20, 196, 186, 23);
-		getContentPane().add(radioButton_4);
+		JRadioButton rdbtnEmployeeInA = new JRadioButton("Employee in a mascot costume (500$)");
+		rdbtnEmployeeInA.setBounds(239, 87, 248, 23);
+		getContentPane().add(rdbtnEmployeeInA);
 
-		JRadioButton radioButton_5 = new JRadioButton(storeItems[5]);
-		radioButton_5.setBounds(207, 87, 217, 23);
-		getContentPane().add(radioButton_5);
+		JRadioButton rdbtnNewspaperArticle = new JRadioButton("Newspaper article (600$)");
+		rdbtnNewspaperArticle.setBounds(239, 116, 248, 23);
+		getContentPane().add(rdbtnNewspaperArticle);
 
-		JRadioButton radioButton_6 = new JRadioButton(storeItems[6]);
-		radioButton_6.setBounds(207, 116, 217, 23);
-		getContentPane().add(radioButton_6);
+		JRadioButton rdbtnFancyTableCloths = new JRadioButton("Fancy table cloths (1000$)");
+		rdbtnFancyTableCloths.setBounds(239, 144, 248, 23);
+		getContentPane().add(rdbtnFancyTableCloths);
 
-		JRadioButton radioButton_7 = new JRadioButton(storeItems[7]);
-		radioButton_7.setBounds(207, 144, 217, 23);
-		getContentPane().add(radioButton_7);
+		JRadioButton rdbtnCreateAWebsite = new JRadioButton("Create a website (1500$)");
+		rdbtnCreateAWebsite.setBounds(239, 170, 248, 23);
+		getContentPane().add(rdbtnCreateAWebsite);
 
-		JRadioButton radioButton_8 = new JRadioButton(storeItems[8]);
-		radioButton_8.setBounds(207, 170, 217, 23);
-		getContentPane().add(radioButton_8);
+		JRadioButton rdbtnForgeYelpReviews = new JRadioButton("Forge Yelp reviews (2000$)");
+		rdbtnForgeYelpReviews.setBounds(239, 196, 248, 23);
+		getContentPane().add(rdbtnForgeYelpReviews);
 
-		JRadioButton radioButton_9 = new JRadioButton(storeItems[9]);
-		radioButton_9.setBounds(207, 196, 217, 23);
-		getContentPane().add(radioButton_9);
-
-		managerGroup.add(radioButton);
-		managerGroup.add(radioButton_1);
-		managerGroup.add(radioButton_2);
-		managerGroup.add(radioButton_3);
-		managerGroup.add(radioButton_4);
-		managerGroup.add(radioButton_5);
-		managerGroup.add(radioButton_6);
-		managerGroup.add(radioButton_7);
-		managerGroup.add(radioButton_8);
-		managerGroup.add(radioButton_9);
+		//radio buttons are all added to a group so that only one can be selected at a time
+		managerGroup.add(rdbtnPottedPlants);
+		managerGroup.add(rdbtnHigherQualityFood);
+		managerGroup.add(rdbtnProvideFreeMeals);
+		managerGroup.add(rdbtnHoldACharity);
+		managerGroup.add(rdbtnSponserLocalSports);
+		managerGroup.add(rdbtnEmployeeInA);
+		managerGroup.add(rdbtnNewspaperArticle);
+		managerGroup.add(rdbtnFancyTableCloths);
+		managerGroup.add(rdbtnCreateAWebsite);
+		managerGroup.add(rdbtnForgeYelpReviews);
 		
 		
 		
 		JTextPane txtpnEvent = new JTextPane();
-		txtpnEvent.setText("Event: " + event);
-		txtpnEvent.setBounds(10, 226, 413, 102);
-		getContentPane().add(txtpnEvent);
+		txtpnEvent.setText("Event: " + event);//sets the text to be displayed
+		txtpnEvent.setBounds(10, 226, 477, 102);//sets the size of the text box
+		getContentPane().add(txtpnEvent);//adds text to the text box
 
-		JButton btnNext = new JButton("Next");
-		btnNext.setBounds(334, 340, 89, 23);
-		getContentPane().add(btnNext);
+		JButton btnNext = new JButton("Next");//display text for the "next" button
+		btnNext.setBounds(398, 340, 89, 23);//size of the button
+		getContentPane().add(btnNext);//adds text to the button
 		
 		btnNext.addActionListener(new ActionListener() {
+			/**
+			 * method runs when the next button is pushed and randomly displays and activates an event
+			 */
 			public void actionPerformed(ActionEvent arg0) {
 				
-				if (radioButton.isSelected())
+				//If else statements check which radio button ended up being selected and implements its effects on the score
+				if (rdbtnPottedPlants.isSelected())
 				{
 					budget -= 500;
 					satisfaction +=5;
 				}
-				else if (radioButton_1.isSelected())
+				else if (rdbtnHigherQualityFood.isSelected())
 				{
 					budget -= 600;
 					satisfaction +=10;
 				}
-				else if (radioButton_2.isSelected())
+				else if (rdbtnProvideFreeMeals.isSelected())
 				{
 					budget -= 1000;
 					satisfaction += 15;
 				}
-				else if (radioButton_3.isSelected())
+				else if (rdbtnHoldACharity.isSelected())
 				{
 					budget -= 1500;
 					satisfaction +=15;
 					fame += 5;
 				}
-				else if (radioButton_4.isSelected())
+				else if (rdbtnSponserLocalSports.isSelected())
 				{
 					budget -= 2000;
 					satisfaction +=15;
 					fame += 10;
 				}
-				else if (radioButton_5.isSelected())
+				else if (rdbtnEmployeeInA.isSelected())
 				{
 					budget -= 500;
 					fame +=5;
 				}
-				else if (radioButton_6.isSelected())
+				else if (rdbtnNewspaperArticle.isSelected())
 				{
 					budget -= 600;
 					fame +=10;
 				}
-				else if (radioButton_7.isSelected())
+				else if (rdbtnFancyTableCloths.isSelected())
 				{
 					budget -= 1000;
 					fame +=15;
 				}
-				else if (radioButton_8.isSelected())
+				else if (rdbtnCreateAWebsite.isSelected())
 				{
 					budget -= 1500;
 					fame +=15;
 					satisfaction += 5;
 				}
-				else if (radioButton_9.isSelected())
+				else if (rdbtnForgeYelpReviews.isSelected())
 				{
 					budget -= 2000;
 					fame +=15;
 					satisfaction += 10;
 				}
 				
+				//switch chooses a random event based upon a randomly calculated number (see randnum), displays and implements it into the scores
 				switch (randnum())
 				{
 				case 0:
@@ -277,42 +284,52 @@ public class ManagerTurns extends JDialog {
 					break;
 				}
 				
+				//checks if the user has won or lost the game
+				isEnd(satisfaction, fame, budget);
+				
+				//the next block opens a new manager turn window for the next turn
 				ManagerTurns newWindow = new ManagerTurns();
 				newWindow.setVisible(true);
 				setVisible(false);
-				/*txtpnYourCurrentBudget.setText("Your current budget: " + budget + "$");
-				txtpnEvent.setText("Event: " + event);
-				txtpnSatisfaction.setText("Satisfaction: " + satisfaction);
-				txtpnFame.setText("Fame:" + fame);
-				txtpnEvent.setText("Event: " + event);*/
 			}
 		
 		});
 		
-		isEnd(satisfaction, fame, budget);
+		
 	}
 	
+	/**
+	 * Randomly generates a number between 0-9 (inclusive)
+	 * @return the random number
+	 */
 	public int randnum ()
 	{
 		Random rand = new Random();
-		int randomNum = rand.nextInt((9 - 0) + 1) + 0;
+		int randomNum = rand.nextInt((9 - 0) + 1) + 0;//random number is determined
 		return randomNum;
 		
 	}
 	
+	/**
+	 * Checks if the game is voer
+	 * @param sat the satisfaction score
+	 * @param fame the fame score
+	 * @param budget the budget amount
+	 */
 	public void isEnd (int sat, int fame, int budget)
 	{
-		if (sat == 0 || fame == 0 || budget == 0)
+		//checks if any of the scores are less than or equal to zero (then the user loses) or fame or satisfaction is greater than 100 (user wins)
+		if (sat <= 0 || fame <= 0 || budget <= 0)
 		{
-			ManagerLoseGame newWindow = new ManagerLoseGame();
+			ManagerLoseGame newWindow = new ManagerLoseGame();//opens a lose game window
 			newWindow.setVisible(true);
-			setVisible(false);
+			setVisible(false);//hides current window
 		}
-		else if (sat == 100 || fame == 100)
+		else if (sat >= 100 || fame >= 100)
 		{
-			ManagerWinGame newWindow = new ManagerWinGame();
+			ManagerWinGame newWindow = new ManagerWinGame();//opens a win game window
 			newWindow.setVisible(true);
-			setVisible(false);
+			setVisible(false);//hides current window
 		}
 	}
 }
