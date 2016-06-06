23e2ec67e139ac76e70f427053aa7c5392c92ff2
clickbug2 <clickbug2@192.168.2.22>
2016-06-05 23:39:20 -0400

    done commenting rules

--- Restaurant Simulator/src/project/Rules.java
+++ Restaurant Simulator/src/project/Rules.java
@@ -66,7 +66,7 @@ public class Rules extends JDialog {
 		contentPanel.setLayout(null);
 		{
 			JButton okButton = new JButton("OK");//creates the "OK button"
-			okButton.setBounds(377, 227, 47, 23);
+			okButton.setBounds(377, 227, 47, 23);//sets dimensions
 			contentPanel.add(okButton);
 			okButton.setActionCommand("OK");
 			getRootPane().setDefaultButton(okButton);
@@ -75,19 +75,19 @@ public class Rules extends JDialog {
 			JButton btnOk = new JButton("OK");
 			btnOk.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					setVisible(false);
+					setVisible(false);//hides window when button is pushed
 				}
 			});
-			btnOk.setBounds(345, 238, 89, 23);
+			btnOk.setBounds(345, 238, 89, 23);//sets dimensions
 			getContentPane().add(btnOk);
 		}
 		
 		JTextPane txtpnSdfhdzfbSzdFdfg = new JTextPane();
-		txtpnSdfhdzfbSzdFdfg.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		txtpnSdfhdzfbSzdFdfg.setEditable(false);
+		txtpnSdfhdzfbSzdFdfg.setFont(new Font("Tahoma", Font.PLAIN, 11));//sets text size and fonts
+		txtpnSdfhdzfbSzdFdfg.setEditable(false);//makes it non-editable
 		txtpnSdfhdzfbSzdFdfg.setBackground(SystemColor.control);
-		txtpnSdfhdzfbSzdFdfg.setText("If you are a cashier:\r\n\r\nYour goal is to enter the totals from a customer's receipt into your cash register as fast as possible so as to be able to raise your customer satisfaction score to 100 (you start at 50). If your score falls to 0, then you lose, if it goes to 100, you win! Once you have completed a receipt you will receive a tip based upon that customer's rating of you. You can use these tips to purchase powerups that will give you a little extra time.\r\n\r\nIf you are a manager:\r\n\r\nYour goal is to get your customer satisfaction and restaurant fame levels from 50 (the starting point) to 100. If these levels drop to 0, then you lose. You will start with a budget of $10 000 and you will be able to invest this money in different objects, all of which will have different effects. At the end of every turn you may lose or earn money, so be careful with your investments!");
-		txtpnSdfhdzfbSzdFdfg.setBounds(10, 11, 414, 250);
+		txtpnSdfhdzfbSzdFdfg.setText("If you are a cashier:\r\n\r\nYour goal is to enter the totals from a customer's receipt into your cash register as fast as possible so as to be able to raise your customer satisfaction score to 100 (you start at 50). If your score falls to 0, then you lose, if it goes to 100, you win! Once you have completed a receipt you will receive a tip based upon that customer's rating of you. You can use these tips to purchase powerups that will give you a little extra time.\r\n\r\nIf you are a manager:\r\n\r\nYour goal is to get your customer satisfaction and restaurant fame levels from 50 (the starting point) to 100. If these levels drop to 0, then you lose. You will start with a budget of $10 000 and you will be able to invest this money in different objects, all of which will have different effects. At the end of every turn you may lose or earn money, so be careful with your investments!");//rules are displayed
+		txtpnSdfhdzfbSzdFdfg.setBounds(10, 11, 414, 250);//sets dimensions
 		getContentPane().add(txtpnSdfhdzfbSzdFdfg);
 	}
 }
